d72fd09c7bf9168f32604c5c53ec352a56f2e563
baimamboukar <baimamboukar@gmail.com>
2021-09-24 12:59:46 +0100

    auth repo added

---
+++ lib/repositories/auth_repository.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:home_cooking/models/models.dart';
+
+abstract class AuthRepository {
+  Stream<User?> get getAuthUser;
+  Future<void> signupUser(
+      {required String mail,
+      required String pass,
+      required Cuisinier cuisinier});
+}
