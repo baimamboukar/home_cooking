e8faa413cdf3ac934adc510c4a054b16c530945e
baimamboukar <baimamboukar@gmail.com>
2021-09-24 13:01:48 +0100

    added curved for mobile as well

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/utils.dart';
+import 'package:home_cooking/models/models.dart';
+import 'package:home_cooking/providers/auth_providers.dart';
 import 'package:home_cooking/utils/palette.dart';
 import 'package:home_cooking/utils/styles.dart';
 import 'package:home_cooking/utils/utils.dart';
@@ -12,12 +14,15 @@ import 'package:line_icons/line_icons.dart';
 class Signup extends ConsumerWidget {
   const Signup({Key? key}) : super(key: key);
   static const routeName = '/signup';
+  static final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context, ScopedReader watch) {
     final emailController = TextEditingController();
     final usernameController = TextEditingController();
     final passwordController = TextEditingController();
-    final _formKey = GlobalKey<FormState>();
+    final adressController = TextEditingController();
+
+    final _auth = watch(authProvider);
 
     return Scaffold(
         backgroundColor: Palette.primary,
@@ -36,10 +41,15 @@ class Signup extends ConsumerWidget {
                   )),
               Container(
                 decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 25.0,
+                          spreadRadius: 6.0,
+                          color: Palette.light)
+                    ],
                     color: Palette.light,
-                    borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(45.0),
-                        topRight: Radius.circular(45.0))),
+                    borderRadius: BorderRadius.vertical(
+                        top: Radius.elliptical(Screen.width(context), 100.0))),
                 child: Form(
                     key: _formKey,
                     child: Column(children: [
@@ -75,6 +85,21 @@ class Signup extends ConsumerWidget {
                         },
                       ),
                       Input(
+                        icon: LineIcons.mapMarked,
+                        context: context,
+                        type: TextInputType.text,
+                        controller: adressController,
+                        label: "adress",
+                        hint: "Enter your adress",
+                        isPassword: false,
+                        isPhone: false,
+                        validator: (value) {
+                          return !(value!.length > 8)
+                              ? "Entrez une adresse valide"
+                              : null;
+                        },
+                      ),
+                      Input(
                         icon: LineIcons.lockOpen,
                         context: context,
                         type: TextInputType.text,
@@ -92,7 +117,16 @@ class Signup extends ConsumerWidget {
                       const SizedBox(height: 10.0),
                       GestureDetector(
                         onTap: () {
-                          _formKey.currentState?.validate();
+                          if (_formKey.currentState!.validate()) {
+                            Cuisinier _cuisinier = Cuisinier(
+                                name: usernameController.value.text,
+                                email: emailController.value.text,
+                                adress: passwordController.value.text);
+                            _auth.signupUser(
+                                mail: emailController.value.text,
+                                pass: passwordController.value.text,
+                                cuisinier: _cuisinier);
+                          }
                         },
                         child: Button(
                           label: "Signup",
