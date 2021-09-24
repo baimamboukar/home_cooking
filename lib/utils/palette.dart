9799f3d10deb00f3e662d64ecd32c63b1c59fff1
baimamboukar <baimamboukar@gmail.com>
2021-09-24 05:51:28 +0100

    palette color added

---
+++ lib/utils/palette.dart
@@ -0,0 +1,27 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class Palette {
+  static Color primary = Colors.teal;
+  static Color secondary = Colors.orange;
+  static Color success = Colors.green;
+  static Color error = Colors.redAccent;
+  static Color light = Colors.white;
+  static Color dark = Colors.black45;
+  static LinearGradient linearGradient = LinearGradient(colors: [
+    Colors.teal,
+    Colors.teal.withOpacity(0.7),
+    Colors.teal.withOpacity(0.4)
+  ]);
+  static LinearGradient gradientWith(Color color) => LinearGradient(
+      colors: [color, color.withOpacity(0.5), color.withOpacity(0.3)]);
+
+  static LinearGradient customGradientWith(List<Color> colors) =>
+      LinearGradient(
+          colors: colors,
+          begin: Alignment.topLeft,
+          end: Alignment.bottomCenter);
+  static Color randomColor() =>
+      Colors.primaries[Random().nextInt(Colors.primaries.length)];
+}
