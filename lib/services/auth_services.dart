0874761652bd03e473465afbadd425a87ef86f30
baimamboukar <baimamboukar@gmail.com>
2021-09-25 12:40:53 +0100

    login working

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -2,11 +2,13 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get/get.dart';
 import 'package:home_cooking/models/models.dart';
 import 'package:home_cooking/providers/database_providers.dart';
 import 'package:home_cooking/repositories/repositories.dart';
 
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:home_cooking/services/services.dart';
 
 class AuthService extends AuthRepository {
   final FirebaseAuth _auth;
@@ -43,6 +45,22 @@ class AuthService extends AuthRepository {
       await _auth.signOut();
     } on FirebaseAuthException catch (err) {
       print("hoha");
+    }
+  }
+
+  @override
+  //login user and provide his data to the app once done
+  Future<void> loginUser({required String mail, required String pass}) async {
+    try {
+      await _auth
+          .signInWithEmailAndPassword(email: mail, password: pass)
+          .then((response) async {
+        if (_read(databaseProvider).getUser(response.user!.uid)) {
+          Get.offAllNamed('/');
+        }
+      });
+    } on FirebaseAuthException catch (err) {
+      print("buzzz");
     }
   }
 }
