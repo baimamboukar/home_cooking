cc0454fa8eb639c9d200d60b97860f7324c888af
baimamboukar <baimamboukar@gmail.com>
2021-09-29 10:22:52 +0100

    data utils added

---
+++ lib/utils/data.dart
@@ -0,0 +1,53 @@
+import 'package:home_cooking/models/models.dart';
+import 'package:home_cooking/models/plat.dart';
+
+List<Plat> plats = [
+  Plat(
+      imageURL: "assets/images/image1.jpg",
+      price: 8.50,
+      name: "Haricot Magic",
+      cuisiner: Cuisinier(
+          adress: 'Paris 526',
+          email: 'cuisinierpro@homecooking.fr',
+          name: 'Cuisinier Pro')),
+  Plat(
+      imageURL: "assets/images/image2.jpg",
+      price: 6.40,
+      name: "Gateau Magic",
+      cuisiner: Cuisinier(
+          adress: 'Toulouse poste',
+          email: 'cooker@homecooking.fr',
+          name: 'Darwin Jules')),
+  Plat(
+      imageURL: "assets/images/image1.jpg",
+      price: 8.50,
+      name: "Pommes Magic",
+      cuisiner: Cuisinier(
+          adress: 'Marseille, 219',
+          email: 'christophe@homecooking.fr',
+          name: 'Christophe Cuisinier')),
+  Plat(
+      imageURL: "assets/images/image1.jpg",
+      price: 8.50,
+      name: "Haricot Magic",
+      cuisiner: Cuisinier(
+          adress: 'Paris 526',
+          email: 'cuisinierpro@homecooking.fr',
+          name: 'Cuisinier Pro')),
+  Plat(
+      imageURL: "assets/images/image2.jpg",
+      price: 6.40,
+      name: "Haricot Magic",
+      cuisiner: Cuisinier(
+          adress: 'Toulouse poste',
+          email: 'cooker@homecooking.fr',
+          name: 'Darwin Jules')),
+  Plat(
+      imageURL: "assets/images/image1.jpg",
+      price: 8.50,
+      name: "Haricot Magic",
+      cuisiner: Cuisinier(
+          adress: 'Marseille, 219',
+          email: 'christophe@homecooking.fr',
+          name: 'Christophe Cuisinier'))
+];
