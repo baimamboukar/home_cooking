6d87b0c5f0bf8145cae0834f222b4de356f8bed5
baimamboukar <baimamboukar@gmail.com>
2021-09-29 10:20:15 +0100

    home modified

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
 import 'package:home_cooking/providers/auth_providers.dart';
@@ -113,46 +114,10 @@ Widget tab() {
       const SizedBox(
         height: 10.0,
       ),
-      Container(
-        height: 200.0,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return food();
-          },
-        ),
-      ),
+      for (int i = 0; i < plats.length; i++) PlatCaption(plat: plats[i]),
       Center(
           child: Text(
               useProvider(cuisinierControllerProvider.notifier).cuisinier.name))
     ],
   ));
-}
-
-Widget food() {
-  return Padding(
-    padding: const EdgeInsets.all(14.0),
-    child: Card(
-      color: Palette.primary.withOpacity(0.2),
-      child: Container(
-          height: 120.0,
-          width: 200.0,
-          child: Column(
-            children: [
-              const SizedBox(height: 20.0),
-              Image.asset("assets/images/flutter_logo.png"),
-              const Text("Nom de la recette"),
-              const Text("Cuisinier pro"),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text("Prix: \$12.0"),
-                  Icon(LineIcons.shoppingBag),
-                ],
-              )
-            ],
-          )),
-    ),
-  );
 }
