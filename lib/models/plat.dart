1f28fc9a16625793e440238f4ab7839fdd2ba7cf
baimamboukar <baimamboukar@gmail.com>
2021-09-30 09:45:03 +0100

    description added to plat model

--- lib/models/plat.dart
+++ lib/models/plat.dart
@@ -5,10 +5,12 @@ class Plat {
   final double price;
   final String imageURL;
   final String name;
+  final String description;
   Plat({
     required this.cuisiner,
     required this.price,
     required this.imageURL,
     required this.name,
+    required this.description,
   });
 }
